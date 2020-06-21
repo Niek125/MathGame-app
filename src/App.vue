ec9855db823048db7e60eb6043e5488ad3c56768
Niek125 <niek.sleddens@gmail.com>
2020-06-21 22:15:53 +0200

    clickable screen

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
+    <game-screen></game-screen>
   </div>
 </template>
 
 <script>
+import GameScreen from '@/views/GameScreen'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { GameScreen }
 }
 </script>
 
-<style></style>
+<style>
+.flexbox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
+<style scoped>
+#app {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/GameScreen.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="flexbox game-screen">
+    <div>
+      <div class="flexbox question-wrapper">
+        <div class="question">{{ questions[questionIndex].question }}</div>
+      </div>
+      <div class="button-grid">
+        <outlined-button
+          v-for="(data, index) in questions[questionIndex].answers"
+          :key="'answer:' + index"
+          :text="data"
+          :style="'grid-area: answer$index;'"
+          @click.native="answer"
+        ></outlined-button>
+      </div>
+    </div></div
+></template>
+
+<script>
+import OutlinedButton from '@/components/buttons/OutlinedButton'
+export default {
+  name: 'GameScreen',
+  components: { OutlinedButton },
+  data() {
+    return {
+      questions: [
+        { question: '1 + 1', answers: ['1', '2', '3', '4'] },
+        { question: '2 + 2', answers: ['2', '3', '4', '5'] }
+      ],
+      questionIndex: 0
+    }
+  },
+  methods: {
+    answer: function () {
+      if (this.questionIndex >= this.questions.length - 1) {
+        alert('end')
+        return
+      }
+      this.questionIndex++
+    }
+  }
+}
+</script>
+
+<style scoped>
+.game-screen {
+  width: 100%;
+  height: 100%;
+}
+
+.question {
+  font-size: 10vh;
+}
+
+.question-wrapper {
+  margin: 12px;
+}
+
+.button-grid {
+  display: grid;
+  grid-template-columns: auto auto;
+  grid-gap: 12px;
+}
+</style>
