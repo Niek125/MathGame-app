34eecd9de69597504d308a36a43323e5d8a95053
Niek125 <niek.sleddens@gmail.com>
2020-06-28 14:53:59 +0200

    prod config

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -2,8 +2,13 @@ import ApolloClient from 'apollo-client'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 
+let url = 'http://localhost:4000'
+if (process.env.NODE_ENV === 'production') {
+  url = 'https://ncos8docker1.sleddens.lan:7001'
+}
+
 const link = createHttpLink({
-  uri: 'http://localhost:4000/api/graphql'
+  uri: url + '/api/graphql'
 })
 
 const cache = new InMemoryCache()
